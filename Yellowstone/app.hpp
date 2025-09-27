c944b7ec4a67564fea86b63e9a603bd46a2c5988
Sharvil Limaye <lim.sharvil@gmail.com>
2025-09-26 22:25:12 -0400

    Add basic shader support (part 1)

--- Yellowstone/app.hpp
+++ Yellowstone/app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "yellowstone_window.hpp"
+#include "yellowstone_pipeline.hpp"
 
 namespace yellowstone {
 	class App {
@@ -10,5 +11,6 @@ namespace yellowstone {
 		void run();
 	private:
 		YellowstoneWindow yellowstoneWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
+		YellowstonePipeline yellowstonePipeline{ "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv" };
 	};
 }
