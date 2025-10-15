3211695193cbfe65240808ba78e004a94a227c1f
Sharvil Limaye <lim.sharvil@gmail.com>
2025-10-15 18:06:22 -0400

    add build system and shader compiler

--- src/shaders/compile.bat
+++ src/shaders/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.4.321.1\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
-C:\VulkanSDK\1.4.321.1\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+C:\VulkanSDK\1.4.321.1\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
+C:\VulkanSDK\1.4.321.1\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
 pause
